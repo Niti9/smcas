bae18457135d9d70b809341fd7b5824d198fc18e
Niti9 <nnegi8298@gmail.com>
2024-01-18 18:14:42 +0530

    again try to do very big changes in layout and page.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,7 @@
-import Courses from "@/components/Courses";
 import "./globals.css";
-
-import Hero from "@/components/Hero";
-import CourseList from "@/components/CourseList";
 import Navbar from "@/components/Navbar";
-import Test from "@/components/Test";
-import About from "@/components/About";
-import Contact from "@/components/Contact";
-import Strategy from "@/components/Strategy";
 import Footer from "@/components/Footer";
+// import  page  from "./page";
 
 export const metadata = {
   title: "SMCAS",
@@ -23,29 +16,12 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
       <Navbar/>
-        <main
-          className="relative overflow-hidden  
-      p-[1.5rem]
-      md:pl-20 md:pr-20 md:pt-20 xl:pl-20
-       "
-        >
+      <main>
+
+  {children}
        
-          <Hero />
-          <Courses />
+
         </main>
-
-        <div className="relative overflow-hidden ">
-
-          <div className="
-         md:px-8 md:py-4 ">
-          <CourseList />
-          </div>
-
-          <Test />
-          <About />
-          <Strategy/>
-          <Contact/>
-        </div>
           <Footer/>
       </body>
     </html>
--- app/page.js
+++ app/page.js
@@ -1,20 +1,42 @@
-// import Image from 'next/image'
-// import Hero from '@/components2/Hero'
-// import Courses from '@/components2/Courses'
-// import CourseList from '@/components2/CourseList'
+import React from "react";
 
-// import ProgramDetails from '@/components2/ProgramDetails'
-// import Strategy from '@/components/Strategy'
-// import Navbar from '@/components/Navbar'
+import Courses from "@/components/Courses";
+import Hero from "@/components/Hero";
+import CourseList from "@/components/CourseList";
 
-// export default function Home() {
-//   return (
-//    <>
-//     {/* <Hero/>
-//     <Courses/>
-//     <CourseList/> */}
-//    <Navbar/>
-   
-//     </>
-//   )
-// }
+import Test from "@/components/Test";
+import About from "@/components/About";
+import Contact from "@/components/Contact";
+import Strategy from "@/components/Strategy";
+
+const page = () => {
+  return (
+    <>
+      <main
+        className="relative overflow-hidden  
+      p-[1.5rem]
+      md:pl-20 md:pr-20 md:pt-20 xl:pl-20
+       "
+      >
+        <Hero />
+        <Courses />
+      </main>
+
+      <div className="relative overflow-hidden ">
+        <div
+          className="
+         md:px-8 md:py-4 "
+        >
+          <CourseList />
+        </div>
+
+        <Test />
+        <About />
+        <Strategy />
+        <Contact />
+      </div>
+    </>
+  );
+};
+
+export default page;
