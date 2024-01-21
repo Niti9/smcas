8cbe1aaae0804d3aea32219aea1a2e574376679c
Niti9 <nnegi8298@gmail.com>
2024-01-21 10:08:33 +0530

    seo with footer links

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
@@ -6,23 +7,23 @@ import Footer from "@/components/Footer";
 export const metadata = {
   title: "SMCAS",
   description: "Website for SMCAS",
+  other:{
+    'theme-color':'#0d1117',
+    'og:url':'smcas.in',
+    'og:image':'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
+    'og:type':'website',
+
+  }
 };
 
 export default function RootLayout({ children }) {
   return (
-    
-
     /**Mobile responsive code */
     <html lang="en">
       <body>
-      <Navbar/>
-      <main>
-
-  {children}
-       
-
-        </main>
-          <Footer/>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
