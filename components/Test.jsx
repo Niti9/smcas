9ef5c808f5b9a5e38953dbc6a181d62b75ff2e9d
Niti9 <nnegi8298@gmail.com>
2024-01-23 21:49:04 +0530

    changes

--- components/Test.jsx
+++ components/Test.jsx
@@ -9,7 +9,7 @@ const Test = () => {
       <div  id="PrelimsTest" className=" pt-8 pb-4">
         <h1 className="font-bold text-4xl text-center md:text-[38px] text-[#2C3131]
          font-rubik font-feature-settings
-        bg-white flex-col px-4" >
+        bg-white flex-col sm:px-4" >
           Prelims Test Series
         </h1>
       </div>
