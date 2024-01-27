975e1e1ca905e9d1d5c3f593655047a242ee9c31
Niti9 <nnegi8298@gmail.com>
2024-01-28 00:01:34 +0530

    change

--- app/Actions/formAction.js
+++ app/Actions/formAction.js
@@ -62,6 +62,7 @@ import { Resend } from "resend";
 export async function handleFormSubmit(prev, formdata) {
   "use server";
 
+  
   // //API key of Ram an
   // const resend = new Resend("re_9HjtvNQ8_4xyjeK32tFQPa5gCbfGdKvux");
   const resend = new Resend("re_BamMn4An_EDNX9S8mFJWz5cZUGnpwdi5b");
@@ -80,27 +81,43 @@ export async function handleFormSubmit(prev, formdata) {
 
   // Creating a template with the email
 
-  resend.emails.send({
-    from: "contact@smcas.in",
-    to: "contact@smcas.in",
-    subject: "Course Related Query",
-    html: `<h1>New Course Query</h1>
-  <p>Hello SMCAS Admin ,</p>
-  <p>You have received a new query regarding the courses. Here are the details:</p>
-      
-      <div class="question">
-      <p class="user-info">User Name: ${formdata.get("name")}</p>
-      <p class="user-info">User Email: ${formdata.get("email")}</p>
-      <p><strong>Query:  ${formdata.get("description")}</strong></p>
-     ${prev}
+  try{
 
-    <p>Best regards,<br> The Course Support Team</p>`,
-  });
+    resend.emails.send({
+      from: "contact@smcas.in",
+      to: "contact@smcas.in",
+      subject: "Course Related Query",
+      html: `<h1>New Course Query</h1>
+    <p>Hello SMCAS Admin ,</p>
+    <p>You have received a new query regarding the courses. Here are the details:</p>
+        
+        <div class="question">
+        <p class="user-info">User Name: ${formdata.get("name")}</p>
+        <p class="user-info">User Email: ${formdata.get("email")}</p>
+        <p><strong>Query:  ${formdata.get("description")}</strong></p>
+       ${prev}
+  
+      <p>Best regards,<br> The Course Support Team</p>`,
+    });
 
-  console.log("FORM DATA IS ", formdata);
-  // console.log("prev DATA IS ", prev);
+    return {
+      message: "Email sent successfully"
+    };
+  }
+  
 
-  return {
-    message: "email sent successfully",
-  };
+  catch(error){
+
+    console.log("FORM DATA IS ", formdata);
+    // console.log("prev DATA IS ", prev);
+
+    console.error("Error sending emails",error);
+
+    return{
+      message:"Failed to send email", 
+      error: error.message
+    }
+  }
+
+ 
 }
