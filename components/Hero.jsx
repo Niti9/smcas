460ad221a92a969115417977c3c7c7b284e294cd
Niti9 <123177246+Niti9@users.noreply.github.com>
2024-04-18 18:18:45 +0530

    Update Hero.jsx
    mistake of not make comment to import extra files

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import Button from "./Button";
-import Link from "next/link";
-import Image from "next/image";
+// import React from "react";
+// import Button from "./Button";
+// import Link from "next/link";
+// import Image from "next/image";
 
 //   return (
 //     <>
