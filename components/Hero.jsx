7f517d18e06e27872b16e50a86103e7dc555f810
Niti9 <123177246+Niti9@users.noreply.github.com>
2024-04-18 17:59:31 +0530

    Update Hero.jsx
    send images to image slider1 file to slide images

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -76,74 +76,112 @@ import Image from "next/image";
 // };
 
 // components/ImageWithOverlay.js
+// const Hero = () => {
+//   return (
+//     <div className="relative">
+//       <Image
+//         src="/student.png" // Update with your image path
+//         alt="Background"
+//         className="w-full h-auto xl:pl-6 xl:w-[1350px] xl:h-[720px] "
+//         width={1350}
+//         height={720}
+        
+//       />
+
+//       <div
+//         className="absolute inset-0 flex items-center justify-center
+//       "
+//       >
+//         <div
+//           className="text-center text-[#FFFFFF]   
+//         lg:pt-20
+//          xl:pl-52 xl:pr-52 xl:pt-[500px] xl:pb-[342px] "
+//         >
+//           <h1
+//             className="  text-[18px] mt-4   px-4 leading-normal font-bold 
+//            sm:text-[36px] md:text-[36px] lg:text-[56px] md:font-bold lg:leading-[66px]
+//            xl:text-[60px]
+//             font-feature-settings 
+//         font-rubik  "
+//           >
+//             UPSC/MPSC/Foundation Integrated Coaching centre
+//           </h1>
+//           <p
+//             className="text-[8px]  px-2  sm:px-4
+//            md:px-12  md:text-[16px] 
+//              lg:text-[20px] xl:pl-0 xl:pr-0 xl:text-[28px]"
+//           >
+//             UPSC Program inauguration at Indira
+//             Institute of Business Management
+//           </p>
+
+//           <div
+//             className="flexCenter  justify-center items-center  text-[12px] pt-1 md:pt-0
+        
+//         md:text-[14px] md:p-2 md:font-normal 
+//         lg:text-[24px]  xl:text-[28px]
+//             "
+//             id="Test"
+//           >
+//             {/* Enroll now button */}
+//             <Link href="https://wa.me/+918605954303">
+//               <button
+//                 type="button"
+//                 title="Enroll Now"
+//                 className=" py-2 px-3 sm:p-4 lg:mt-4 lg:px-6 lg:py-6  
+               
+//                font-feature-settings font-rubik 
+//                bg-[#FF8112] text-white border rounded-full 
+//                border-[#FF8112] 
+               
+//                "
+//               >
+//                 Enroll Now
+//               </button>
+//             </Link>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Hero;
+
+
+
+
+
+/**new code to add image slide**/
+import React from "react";
+import ImageSlider1 from './ImageSlider1';
+
 const Hero = () => {
+
+  const slides = [
+    {
+      image: '/student.png',
+      className: "w-full h-300 xl:px-6 xl:w-[1350px] xl:h-[720px]",
+      text: "UPSC/MPSC/Foundation Integrated Coaching centre",
+
+      id: 'slide1',
+    },
+    {
+      image: '/smcasLandingPage.jpg',
+      className: "w-full h-[420px] sm:w-[768px] sm:h-[500px] lg:w-[1500px] lg:h-[2100px] rounded-3xl",
+
+
+      id: 'slide2',
+    }
+  ];
+
   return (
-    <div className="relative">
-      <Image
-        src="/student.png" // Update with your image path
-        alt="Background"
-        className="w-full h-auto xl:pl-6 xl:w-[1350px] xl:h-[720px] "
-        width={1350}
-        height={720}
-        
-      />
-
-      <div
-        className="absolute inset-0 flex items-center justify-center
-      "
-      >
-        <div
-          className="text-center text-[#FFFFFF]   
-        lg:pt-20
-         xl:pl-52 xl:pr-52 xl:pt-[500px] xl:pb-[342px] "
-        >
-          <h1
-            className="  text-[18px] mt-4   px-4 leading-normal font-bold 
-           sm:text-[36px] md:text-[36px] lg:text-[56px] md:font-bold lg:leading-[66px]
-           xl:text-[60px]
-            font-feature-settings 
-        font-rubik  "
-          >
-            UPSC/MPSC/Foundation Integrated Coaching centre
-          </h1>
-          <p
-            className="text-[8px]  px-2  sm:px-4
-           md:px-12  md:text-[16px] 
-             lg:text-[20px] xl:pl-0 xl:pr-0 xl:text-[28px]"
-          >
-            UPSC Program inauguration at Indira
-            Institute of Business Management
-          </p>
-
-          <div
-            className="flexCenter  justify-center items-center  text-[12px] pt-1 md:pt-0
-        
-        md:text-[14px] md:p-2 md:font-normal 
-        lg:text-[24px]  xl:text-[28px]
-            "
-            id="Test"
-          >
-            {/* Enroll now button */}
-            <Link href="https://wa.me/+918605954303">
-              <button
-                type="button"
-                title="Enroll Now"
-                className=" py-2 px-3 sm:p-4 lg:mt-4 lg:px-6 lg:py-6  
-               
-               font-feature-settings font-rubik 
-               bg-[#FF8112] text-white border rounded-full 
-               border-[#FF8112] 
-               
-               "
-              >
-                Enroll Now
-              </button>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
+<section>
+      <ImageSlider1 slides={slides} interval={3000} />
+</section>
   );
 };
 
 export default Hero;
+
+
